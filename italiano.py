2bb7b59a3bac9bcdf545b88b8a2315f2d3703645
coolarts3 <64877241+coolarts3@users.noreply.github.com>
2025-11-24 20:41:13 +0100

    italiano.py

--- italiano.py
+++ italiano.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"🤖 Bot 2 conectado como {bot.user}")
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send("👋 Hola, soy el segundo bot.")
+
+import os
+bot.run(os.getenv("DISCORD_TOKEN2"))
